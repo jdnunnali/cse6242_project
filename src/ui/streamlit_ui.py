56d538caa9bc9eebe277d8d76690f561eb2db1f1
jdnunnali <42360831+jdnunnali@users.noreply.github.com>
2021-03-22 19:44:55 -0400

    Add files via upload

--- src/ui/streamlit_ui.py
+++ src/ui/streamlit_ui.py
@@ -8,7 +8,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 from streamlit_agraph import agraph, Node, Edge, TripleStore, Config
 
 
-st.title("Food Explorer: A CSE6242 Project")
+st.title("Recipe Recommender Network")
 
 
 testDf = pd.DataFrame({"a":[1,2,3,4,5],
@@ -19,11 +19,17 @@ nodes = []
 edges = []
 nodeSize = 500
 
-nodes.extend([Node(id="Spicy Potatoes",label="Spicy Potatoes",size=nodeSize),
-             Node(id="Spicy Chicken",label="Spicy Chicken",size=nodeSize),
-             Node(id="Pork Belly",label="Pork Belly",size=nodeSize),
-             Node(id="Jamaican Fish",label="Jamaican Fish",size=nodeSize),
-             Node(id="Pickles",label="Pickles",size=nodeSize)])
+recipe_images = {"Spicy Chicken": "https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/37/21/1/picdx8xbY.jpg",
+                 "Spicy Potatoes": "https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/54/57/7/pic1IWdex.jpg",
+                 "Pork Belly": "https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/23/87/73/aeTvkKFkTuKUm5tv1PmH_porkbelly-1470.jpg",
+                 "Jamaican Fish": "https://thatgirlcookshealthy.com/wp-content/uploads/2018/03/Jamaican-steam-fish.jpg",
+                 "Pickles": "https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/18/02/57/picA94k8p.jpg"}
+
+nodes.extend([Node(id="Spicy Potatoes",label="Spicy Potatoes",size=nodeSize, svg="https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/54/57/7/pic1IWdex.jpg"),
+             Node(id="Spicy Chicken",label="Spicy Chicken",size=nodeSize, svg= "https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/37/21/1/picdx8xbY.jpg"),
+             Node(id="Pork Belly",label="Pork Belly",size=nodeSize, svg="https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/23/87/73/aeTvkKFkTuKUm5tv1PmH_porkbelly-1470.jpg"),
+             Node(id="Jamaican Fish",label="Jamaican Fish",size=nodeSize, svg= "https://thatgirlcookshealthy.com/wp-content/uploads/2018/03/Jamaican-steam-fish.jpg"),
+             Node(id="Pickles",label="Pickles",size=nodeSize, svg= "https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/18/02/57/picA94k8p.jpg")])
 
 
 edges.extend([Edge(source="Spicy Potatoes",label ="spicy food", target="Spicy Chicken"),
@@ -36,10 +42,15 @@ edges.extend([Edge(source="Spicy Potatoes",label ="spicy food", target="Spicy Ch
               ])
 
 def app():
-  st.title("Graph Example")
+  st.title("Related Recipes")
+
   sidebar = st.sidebar
-  sidebar.title("Filter By Category")
-  display_category = sidebar.selectbox("Query Type: ",index=0, options = ["all","spicy foods","meats","non-meats"]) # could add more stuff here later on or add other endpoints in the sidebar.
+  middle, rsidebar = st.beta_columns([3,1])
+  sidebar.title("Choose meal type and ingredients")
+
+  display_category = sidebar.selectbox("Cuisine Type: ",index=0, options = ["all","spicy food","meats","non-meats"]) # could add more stuff here later on or add other endpoints in the sidebar.
+  mealType = sidebar.selectbox("Meal Type: ", index=0, options = ["Breakfast", "Lunch", "Dinner"]) # Just a place holder could be 'soup', 'salad', or 'italian', 'indian', etc
+  userInput = sidebar.text_input("Input ingredients to search for: ", "potatoes, tomatoes")
 
   config = Config(height=300,
                   width=700,
@@ -52,32 +63,33 @@ def app():
                   )
 
 
-  st.text(display_category)
-
   if display_category=="all":
       viewEdges = edges
   else:
       viewEdges = [edge for edge in edges if edge.label==display_category]
 
-  return_value = agraph(nodes=nodes,
+  with middle:
+      st.text("Displaying {} cuisine types".format(display_category))
+      return_value = agraph(nodes=nodes,
                       edges=viewEdges,
                       config=config)
 
 
 
-  data_load_state = st.text("Loading Data...")
+  #data_load_state = st.text("Loading Data...")
 
-  st.subheader("Reviewing the Data")
-  st.write(testDf)
+  with rsidebar:
+      st.subheader("Selected recipe")
+      st.image("https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/37/21/1/picdx8xbY.jpg")
+      #st.write(testDf)
 
-  st.subheader("Graphing the Data")
-  st.bar_chart(testDf)
+  #st.subheader("Graphing the Data")
+  #st.bar_chart(testDf)
 
-  checkBox = st.checkbox("Label",value=True,key="on")
+  #checkBox = st.checkbox("Label",value=True,key="on")
 
-  if checkBox == True:
-      st.subheader("Displaying text because checkbox is true")
-  userInput = st.text_input("input keyword ingredients","potatoes, tomatoes")
+  #if checkBox == True:
+      #st.subheader("Displaying text because checkbox is true")
 
   st.text("User selected the following ingredients: "+userInput)
 
